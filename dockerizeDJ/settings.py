e62e7fd437945297b15c59006e37c1fd1e0faad7
Alisjj <aliyussajjads@gmail.com>
2022-06-01 15:12:44 +0100

    error

--- dockerizeDJ/settings.py
+++ dockerizeDJ/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from email.policy import default
 from pathlib import Path
 import os
+
+from django.conf import ENVIRONMENT_VARIABLE
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,7 +142,7 @@ DATABASES = {
     }
 }
 
-db_from_env = str(dj_database_url.config(conn_max_age=500))
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
@@ -186,6 +188,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 PS_SECRET = os.environ.get("PS_SECRET")
 PS_PUBLIC = os.environ.get("PS_PUBLIC")
+ENVIRONMENT = os.environ.get("ENVIRONMENT")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -193,3 +196,15 @@ PS_PUBLIC = os.environ.get("PS_PUBLIC")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+
+if ENVIRONMENT == 'production':
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
