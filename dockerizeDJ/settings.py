f5c9a7fcf8d2329648034084a96a3efe240accdc
Alisjj <aliyussajjads@gmail.com>
2022-06-01 15:02:53 +0100

    data base

--- dockerizeDJ/settings.py
+++ dockerizeDJ/settings.py
@@ -140,7 +140,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = str(dj_database_url.config(conn_max_age=500))
 DATABASES['default'].update(db_from_env)
 
 # Password validation
